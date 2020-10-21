109b9a7bc5d2194ffb22da4494dbc8e05f313b1f
N3Cr0 Cr0W <9968079+n3cr0cr0w@users.noreply.github.com>
2020-10-20 23:26:26 -0500

    Update Amazon.Skips.user.js
    Fix Menu Commands registration and URL.

--- Amazon.Skips.user.js
+++ Amazon.Skips.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Amazon Skips
 // @namespace    https://github.com/N3Cr0Cr0W/userscripts
-// @version      0.3
+// @version      0.4
 // @description  Skips Recap, Intro and Ads. Allows Saved PlaybackRate.
 // @author       N3Cr0Cr0W
 // @downloadURL  https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/Amazon.Skips.user.js
 // @license      GNU AGPLv3: https://www.gnu.org/licenses/agpl-3.0.en.html
-// @match        https://www.amazon.com/*autoplay=1*
+// @match        https://www.amazon.com/gp/video
 // @grant        GM_log
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -24,8 +24,8 @@
 	}
 	function skipsAndStuff(){
 		var playbackRate=GM_getValue('playbackRate',1);
-		var recap=document.evaluate('//div[text()="Skip Recap"]',document,null,9,null).singleNodeValue;
-		var intro=document.evaluate('//div[text()="Skip Intro"]',document,null,9,null).singleNodeValue;
+		var recap=document.evaluate('//button[text()="Skip Recap"]',document,null,9,null).singleNodeValue;
+		var intro=document.evaluate('//button[text()="Skip Intro"]',document,null,9,null).singleNodeValue;
 		var next=document.querySelector("div#dv-web-player>div>div:nth-child(1)>div>div>div.webPlayerSDKPlayerContainer>div>div>div>div>div>div>div>div>div>div>div>div>svg>circle");
 		var ad=document.evaluate('//div[text()="Skip"]',document,null,9,null).singleNodeValue;
 		var video=document.evaluate('//video',document,null,9,null).singleNodeValue;
@@ -52,9 +52,9 @@
 		if(video&&video.playbackRate!=playbackRate){
 			video.playbackRate=playbackRate;
 			GM_log("Set Playback Rate to "+playbackRate+"X.");
-			GM_registerMenuCommand('Faster',increasePlaybackRate,'F');
-			GM_registerMenuCommand('Slower',decreasePlaybackRate,'S');
 		}
 	}
 	var timerID=setInterval(skipsAndStuff,800);
+	GM_registerMenuCommand('Faster',increasePlaybackRate,'F');
+	GM_registerMenuCommand('Slower',decreasePlaybackRate,'S');
 })();
