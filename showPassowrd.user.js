8bda030159d027f7ee303a62a45bb26befe106da
N3Cr0 Cr0W <9968079+n3cr0cr0w@users.noreply.github.com>
2022-10-07 15:42:46 -0500

    Brain barfed a new file, don't use it!

---
+++ showPassowrd.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name          Show Password onMouseOver
+// @namespace     https://github.com/N3Cr0Cr0W/userscripts
+// @description	  Show password when mouseover on password input dom object (Don't use this, lol)
+// @downloadURL  https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/showPassowrd.user.js
+// @author        N3Cr0Cr0W
+// @license       MIT
+// @match		 https://*/*
+// @version 0.1
+// @run-at document-idle
+// ==/UserScript==
+!function(){
+	var passFields=document.querySelectorAll("input[type='password']");
+	if(!passFields.length){
+		return;
+	}
+	for(var i=0;i<passFields.length;i++){
+		passFields[i].addEventListener("mouseover",function(){
+			this.type="text";
+		},false);
+		passFields[i].addEventListener("mouseout",function(){
+			this.type="password";
+		},false);
+	}
+}();
