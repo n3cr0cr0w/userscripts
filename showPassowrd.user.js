7dc21868150b17d8d1bff0d4aa8dbe2cee01b99f
N3Cr0 Cr0W <9968079+n3cr0cr0w@users.noreply.github.com>
2022-10-07 15:49:42 -0500

    Fixed formatting and added update URL.

--- showPassowrd.user.js
+++ showPassowrd.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name          Show Password onMouseOver
-// @namespace     https://github.com/N3Cr0Cr0W/userscripts
-// @description	  Show password when mouseover on password input dom object (Don't use this, lol)
-// @downloadURL  https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/showPassowrd.user.js
-// @author        N3Cr0Cr0W
-// @license       MIT
-// @match		 https://*/*
-// @version 0.1
-// @run-at document-idle
+// @name		Show Password onMouseOver
+// @namespace		https://github.com/N3Cr0Cr0W/userscripts
+// @description		Show password when mouseover on password input dom object (Don't use this, lol)
+// @downloadURL		https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/showPassowrd.user.js
+// @updateURL		https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/showPassowrd.user.js
+// @author		N3Cr0Cr0W
+// @license		MIT
+// @match		https://*/*
+// @version		0.1
+// @run-at		document-idle
 // ==/UserScript==
 !function(){
 	var passFields=document.querySelectorAll("input[type='password']");
