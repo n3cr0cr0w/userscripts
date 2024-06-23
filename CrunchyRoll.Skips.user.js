2ea55c1aaabc3be40d44d7d357f237471ae5a982
N3Cr0 Cr0W <9968079+n3cr0cr0w@users.noreply.github.com>
2024-06-22 19:16:05 -0500

    Update CrunchyRoll.Skips.user.js
    Added Skip Intro

--- CrunchyRoll.Skips.user.js
+++ CrunchyRoll.Skips.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name		 Crunchy Roll Skips
-// @namespace	 https://github.com/N3Cr0Cr0W/userscripts
-// @version		 0.2
-// @description  Plays next episode
-// @author		 N3Cr0Cr0W
-// @downloadURL  https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/CrunchyRoll.Skips.user.js
-// @updateURL    https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/CrunchyRoll.Skips.user.js
-// @match		 https://static.crunchyroll.com/vilos-v2/*
-// @grant		 GM_log
+// @name		Crunchy Roll Skips
+// @namespace		https://github.com/N3Cr0Cr0W/userscripts
+// @version		0.3
+// @description		Plays next episode, Skips Intro
+// @author		N3Cr0Cr0W
+// @downloadURL  	https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/CrunchyRoll.Skips.user.js
+// @updateURL    	https://raw.githubusercontent.com/N3Cr0Cr0W/userscripts/master/CrunchyRoll.Skips.user.js
+// @match		https://static.crunchyroll.com/vilos-v2/*
+// @grant		GM_log
 // ==/UserScript==
 (function(){
 	'use strict';
-	let playbackRate=GM_getValue('playbackRate',2);
 	let urlPathName="";
 	let videoPlayer="";
 	function tEvent(){
@@ -32,14 +31,19 @@
 	}
 	const appMountPoint=document.getElementById("vilos");
 	const mutObserver=new MutationObserver(mutations=>{
-//		GM_log(mutations);
 		mutations.forEach(mutation=>{
 			if(mutation.nextSibling&&mutation.addedNodes.length){
 				Array.from(mutation.addedNodes).filter(node=>{
-					GM_log(node);
 					if(node.className==="end-card-container"){
 						setTimeout(()=>{document.querySelector(".end-card__metadata-area-play-button").click();},200);
 						GM_log('Next Ep');
+					}
+				});
+			}else if(!mutation.nextSibling&&mutation.addedNodes.length==1){
+				Array.from(mutation.addedNodes).filter(node=>{
+					if(node.innerText==="SKIP INTRO"){
+						setTimeout(()=>{node.children[0].click();},200);
+						GM_log('Skip Intro');
 					}
 				});
 			}
